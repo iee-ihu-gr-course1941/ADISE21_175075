25a76e76803b4e4d9a64cfaa7b4dd8016df2c36a
Alexandros Zacharegkas <49199721+AlexZacha@users.noreply.github.com>
2021-12-12 18:31:18 +0200

    index, js folder, css folder
    Creation of index.html, js function for creating and shuffling deck with 45 cards as array in file deck.js.
    
    The file script.js will contain the rules for the game and the css folder will have all the css styles

---
+++ js/deck.js
@@ -0,0 +1,37 @@
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+export function getDeck() {
+	let deck = new Array();
+
+	for(let i = 0; i < SUITS.length; i++)
+	{
+		for(let x = 0; x < VALUES.length; x++)
+		{
+			let card = {Value: VALUES[x], Suit: SUITS[i]};
+			deck.push(card);
+		}
+	}
+
+    let m = {Value: "K", Suit: "♣"}
+    deck.push(m)
+
+    shuffle(deck)
+
+	return deck;
+}
+
+function shuffle(deck)
+{
+	// for 1000 turns
+	// switch the values of two random cards
+	for (let i = 0; i < 1000; i++)
+	{
+		let location1 = Math.floor((Math.random() * deck.length));
+		let location2 = Math.floor((Math.random() * deck.length));
+		let tmp = deck[location1];
+
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
+	}
+}
---
+++ js/script.js
@@ -0,0 +1,5 @@
+import { getDeck } from "./deck.js"
+
+var deck = getDeck()
+
+console.log(deck)
